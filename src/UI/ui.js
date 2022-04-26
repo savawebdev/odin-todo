e7ff523614c82158c9c502aae2b49d64e8a0abf4
savawedev <andrei.sava.dev@gmail.com>
2022-04-26 08:09:39 +0300

    Added function to create new task card

--- src/UI/ui.js
+++ src/UI/ui.js
@@ -1,4 +1,4 @@
-// MODAL
+// Open/close modal
 function toggleModal() {
   const overlay = document.querySelector(".overlay");
   const modal = document.querySelector(".modal");
@@ -7,4 +7,35 @@ function toggleModal() {
   modal.classList.toggle("hidden");
 }
 
-export { toggleModal };
+// Create task card
+function createTaskCard(title, description, dueDate, priority) {
+  const taskCard = document.createElement("div");
+  taskCard.classList.add("card");
+
+  const taskCardTitle = document.createElement("h4");
+  taskCardTitle.classList.add("card-title");
+  taskCardTitle.textContent = title;
+
+  const taskCardDescription = document.createElement("p");
+  taskCardDescription.classList.add("card-description");
+  taskCardDescription.textContent = description;
+
+  const taskCardDueDate = document.createElement("p");
+  taskCardDueDate.classList.add("card-due-date");
+  taskCardDueDate.textContent = dueDate;
+
+  const taskCardPriority = document.createElement("p");
+  taskCardPriority.classList.add("card-priority");
+  taskCardPriority.textContent = priority;
+
+  taskCard.append(
+    taskCardTitle,
+    taskCardDescription,
+    taskCardDueDate,
+    taskCardPriority
+  );
+
+  return taskCard;
+}
+
+export { toggleModal, createTaskCard };
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const newTaskBtn = document.getElementById("new-task-btn");
 const cancelBtn = document.getElementById("cancel-btn");
 const overlay = document.querySelector(".overlay");
 
+// Open/close modal event listener
 [newTaskBtn, cancelBtn, overlay].forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.preventDefault();
